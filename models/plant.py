34cf03f90617a4053ae2de9dbcc758e9a3337bdc
Jho <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-12-29 18:57:48 -0500

    plant file creation

---
+++ models/plant.py
@@ -0,0 +1,6 @@
+class Plant:
+    def __init__(self,id,specie,date,description):
+        self.id = id
+        self.specie = specie
+        self.date_aquired = date
+        self.description = description
