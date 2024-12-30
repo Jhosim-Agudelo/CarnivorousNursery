eb4bcc1c2500875dc54fda6f8963e4e42b2a517c
Jho <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-12-29 20:26:41 -0500

    Add type hints to Plant model constructor parameters

--- models/plant.py
+++ models/plant.py
@@ -1,5 +1,5 @@
 class Plant:
-    def __init__(self,name,specie,date,description):
+    def __init__(self,name: str,specie : str,date: str,description: str):
         self.name = name
         self.specie = specie
         self.date_aquired = date
