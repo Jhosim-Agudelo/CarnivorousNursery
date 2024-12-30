3e4f00a0040ae4cab63b45e007e53e377d6c2253
Jho <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-12-29 19:22:03 -0500

    Rename 'id' to 'name' in Plant model constructor

--- models/plant.py
+++ models/plant.py
@@ -1,6 +1,6 @@
 class Plant:
-    def __init__(self,id,specie,date,description):
-        self.id = id
+    def __init__(self,name,specie,date,description):
+        self.name = name
         self.specie = specie
         self.date_aquired = date
         self.description = description
